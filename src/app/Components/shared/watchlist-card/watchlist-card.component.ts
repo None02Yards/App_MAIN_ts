d1de5c96316051b82309099039022b2414f271b5
Zu34 <gmathi091@gmail.com>
2025-07-04 08:09:33 -0800

    handel msg

--- src/app/Components/shared/watchlist-card/watchlist-card.component.ts
+++ src/app/Components/shared/watchlist-card/watchlist-card.component.ts
@@ -1,8 +1,4 @@
-
-
-//Shared dir
-// watchlist-card// watchlist-card.component.ts
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, HostListener  } from '@angular/core';
 
 @Component({
   selector: 'app-watchlist-card',
@@ -14,7 +10,36 @@ export class WatchlistCardComponent {
   @Input() mediaType: 'movie' | 'tv' = 'movie'; 
   @Output() remove = new EventEmitter<number>();
 
+  // confirmation & feedback state
+  showConfirm = false;
+  pendingId: number | null = null;
+  showRemovedMessage = false;
+
   onRemoveClick(id: number) {
-    this.remove.emit(id);
+    this.pendingId = id;
+    this.showConfirm = true;
+  }
+
+  confirmRemove() {
+    if (this.pendingId != null) {
+      this.remove.emit(this.pendingId);
+      this.showConfirm = false;
+      this.showRemovedMessage = true;
+      // hide after 1.5s
+      setTimeout(() => this.showRemovedMessage = false, 4000);
+    }
+  }
+
+  cancelRemove() {
+    this.showConfirm = false;
+    this.pendingId = null;
+  }
+
+   // <-- new HostListener
+  @HostListener('document:keydown.escape', ['$event'])
+  handleEscape(event: KeyboardEvent) {
+    if (this.showConfirm) {
+      this.cancelRemove();
+    }
   }
 }
